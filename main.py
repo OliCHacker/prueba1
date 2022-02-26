6495134f597b6f2c2e56a5abed6fb9478fe26d68
Cualquier Cosa Te Averiguo <96824508+cualquiercosateaveriguo@users.noreply.github.com>
2022-02-26 20:07:20 -0300

    Propongo cambios
    Acá te hago unas propuestas de cambios, para ver que pasa. Fijate que te parece.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #probando github por primera vez
 print("hola")
 #Te agrego una línea de código. A ver que pasa.
-print('Hola. ¿Que tal?. ¿Todo bien?' 
+print('Hola. ¿Que tal?. ¿Todo bien?')
+print('nueva modificacion')
